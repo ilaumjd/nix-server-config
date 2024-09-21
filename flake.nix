8de20edfe114026b8b86f6bf8a53a4bf62031d70
ilaumjd <ilaumjd@gmail.com>
2024-09-21 22:54:38 +0700

    apply nixfmt

--- flake.nix
+++ flake.nix
@@ -7,11 +7,13 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs =
+    { nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-    in {
+    in
+    {
       homeConfigurations."ilaumjd" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
