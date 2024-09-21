d16616789e472dadcc0bea44bf6a3fee9956230f
ilaumjd <ilaumjd@gmail.com>
2024-09-21 22:29:18 +0700

    add git aliases

--- programs/bash.nix
+++ programs/bash.nix
@@ -6,6 +6,14 @@
     shellAliases = {
       cat = "bat";
       hms = "home-manager switch --flake .";
+
+      # git
+      ga = "git add";
+      gc = "git commit";
+      gcm = "git commit -m";
+      gco = "git checkout";
+      glo = "git log --oneline --graph";
+      gs = "scmpuff status";
     };
   };
 }
